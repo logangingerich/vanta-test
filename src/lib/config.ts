002b0c9d911da9d6e23ad76c59c2b0152639cb8c
Logan Gingerich <logan@speakeasy.com>
2025-01-10 09:38:31 -0500

    update

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -54,7 +54,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "1.0.0",
-  sdkVersion: "0.0.2",
+  sdkVersion: "0.0.3",
   genVersion: "2.492.3",
-  userAgent: "speakeasy-sdk/typescript 0.0.2 2.492.3 1.0.0 logan-vanta",
+  userAgent: "speakeasy-sdk/typescript 0.0.3 2.492.3 1.0.0 logan-vanta",
 } as const;
